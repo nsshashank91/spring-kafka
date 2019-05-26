28b378572bada7769a12d623898aa260029ef0b5
nsshashank91 <nsshashank91@gmail.com>
2019-05-26 11:18:42 +0530

    first commit

---
+++ src/main/java/com/demo/controllers/KafkaController.java
@@ -0,0 +1,27 @@
+package com.demo.controllers;
+
+import com.demo.engine.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    private final Producer producer;
+
+    @Autowired
+    KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @GetMapping(value = "/publish/{message}")
+    public void sendMessageToKafkaTopic(@PathVariable String message) {
+        this.producer.sendMessage(message);
+    }
+}
